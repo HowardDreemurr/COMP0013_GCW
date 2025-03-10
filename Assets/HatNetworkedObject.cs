2b12bec5ad0ed0e753a42d5ec624a26452f6aa54
oliver32768 <oliver32768@gmail.com>
2025-03-10 18:46:45 +0000

    send hat transforms over network

--- Assets/HatNetworkedObject.cs
+++ Assets/HatNetworkedObject.cs
@@ -5,4 +5,43 @@ using Ubiq.Messaging;
 public class HatNetworkedObject : MonoBehaviour, INetworkSpawnable
 {
     public NetworkId NetworkId { get; set; }
+    private NetworkContext context;
+    private Vector3 lastPosition;
+    private Quaternion lastRotation;
+
+    private void Start()
+    {
+        context = NetworkScene.Register(this);
+        lastPosition = transform.localPosition;
+        lastRotation = transform.localRotation;
+    }
+
+    private void Update()
+    {
+        if (transform.localPosition != lastPosition || transform.localRotation != lastRotation)
+        {
+            lastPosition = transform.localPosition;
+            lastRotation = transform.localRotation;
+            context.SendJson(new HatMessage
+            {
+                position = transform.localPosition,
+                rotation = transform.localRotation
+            });
+        }
+    }
+
+    public void ProcessMessage(ReferenceCountedSceneGraphMessage message)
+    {
+        var msg = message.FromJson<HatMessage>();
+        transform.localPosition = msg.position;
+        transform.localRotation = msg.rotation;
+        lastPosition = transform.localPosition;
+        lastRotation = transform.localRotation;
+    }
+
+    private struct HatMessage
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+    }
 }
