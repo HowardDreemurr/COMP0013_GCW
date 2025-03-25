df1c290c225ec63da5b144a8c14e916635c83b11
Howard Cui <78500597+HowardCui-Super@users.noreply.github.com>
2025-03-25 02:35:04 +0000

    Add Sign and Descriptions

---
+++ Assets/AvatarGame/FloatingAvatar.cs
@@ -0,0 +1,165 @@
+﻿using Ubiq;
+using UnityEngine;
+
+/// <summary>
+/// Recroom/rayman style avatar with hands, torso and head. This class is only
+/// here for compatibility. You can safely ignore it for this example/tutorial.
+/// </summary>
+public class FloatingAvatar : MonoBehaviour
+{
+    public Transform head; // Floating_Head
+    public Transform torso; // Floating_Torso_A
+    public Transform leftHand; // Floating_LeftHand_A
+    public Transform rightHand; // Floating_RightHand_A
+
+    public Renderer headRenderer;
+    public Renderer torsoRenderer;
+    public Renderer leftHandRenderer;
+    public Renderer rightHandRenderer;
+
+    public Transform baseOfNeckHint;
+
+    public AnimationCurve torsoFootCurve;
+
+    public AnimationCurve torsoFacingCurve;
+
+    private TexturedAvatar texturedAvatar;
+    private HeadAndHandsAvatar headAndHandsAvatar;
+    private Vector3 footPosition;
+    private Quaternion torsoFacing;
+    
+    private InputVar<Pose> lastGoodHeadPose;
+
+    private void OnEnable()
+    {
+        headAndHandsAvatar = GetComponentInParent<HeadAndHandsAvatar>();
+
+        if (headAndHandsAvatar)
+        {
+            headAndHandsAvatar.OnHeadUpdate.AddListener(HeadAndHandsEvents_OnHeadUpdate);
+            headAndHandsAvatar.OnLeftHandUpdate.AddListener(HeadAndHandsEvents_OnLeftHandUpdate);
+            headAndHandsAvatar.OnRightHandUpdate.AddListener(HeadAndHandsEvents_OnRightHandUpdate);
+        }
+
+        texturedAvatar = GetComponentInParent<TexturedAvatar>();
+
+        if (texturedAvatar)
+        {
+            texturedAvatar.OnTextureChanged.AddListener(TexturedAvatar_OnTextureChanged);
+        }
+
+        // Ensure MeshCollider is enabled on head
+        // We'll need this for putting hats on people since the capsule hitbox for avatars is actually a part of something completely separate to avatars
+        if (head != null)
+        {
+            MeshCollider meshCollider = head.GetComponent<MeshCollider>();
+            if (meshCollider == null)
+            {
+                meshCollider = head.gameObject.AddComponent<MeshCollider>();
+            }
+            meshCollider.convex = true; // Ensures it can be a trigger
+            meshCollider.isTrigger = true; // Enables trigger mode
+            meshCollider.enabled = true;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (headAndHandsAvatar && headAndHandsAvatar != null)
+        {
+            headAndHandsAvatar.OnHeadUpdate.RemoveListener(HeadAndHandsEvents_OnHeadUpdate);
+            headAndHandsAvatar.OnLeftHandUpdate.RemoveListener(HeadAndHandsEvents_OnLeftHandUpdate);
+            headAndHandsAvatar.OnRightHandUpdate.RemoveListener(HeadAndHandsEvents_OnRightHandUpdate);
+        }
+
+        if (texturedAvatar && texturedAvatar != null)
+        {
+            texturedAvatar.OnTextureChanged.RemoveListener(TexturedAvatar_OnTextureChanged);
+        }
+    }
+
+    private void HeadAndHandsEvents_OnHeadUpdate(InputVar<Pose> pose)
+    {
+        if (!pose.valid)
+        {
+            if (!lastGoodHeadPose.valid)
+            {
+                headRenderer.enabled = false;
+                return;
+            }
+            
+            pose = lastGoodHeadPose;
+        }
+        
+        head.position = pose.value.position; 
+        head.rotation = pose.value.rotation;        
+        lastGoodHeadPose = pose;
+    }
+
+    private void HeadAndHandsEvents_OnLeftHandUpdate(InputVar<Pose> pose)
+    {
+        if (!pose.valid)
+        {
+            leftHandRenderer.enabled = false;
+            return;
+        }
+        
+        leftHandRenderer.enabled = true;
+        leftHand.position = pose.value.position;
+        leftHand.rotation = pose.value.rotation;                    
+    }
+
+    private void HeadAndHandsEvents_OnRightHandUpdate(InputVar<Pose> pose)
+    {
+        if (!pose.valid)
+        {
+            rightHandRenderer.enabled = false;
+            return;
+        }
+
+        rightHandRenderer.enabled = true;
+        rightHand.position = pose.value.position;
+        rightHand.rotation = pose.value.rotation;                    
+    }
+
+    private void TexturedAvatar_OnTextureChanged(Texture2D tex)
+    {
+        headRenderer.material.mainTexture = tex;
+        torsoRenderer.material = headRenderer.material;
+        leftHandRenderer.material = headRenderer.material;
+        rightHandRenderer.material = headRenderer.material;
+    }
+
+    private void Update()
+    {
+        UpdateTorso();
+    }
+
+    private void UpdateTorso()
+    {
+        // Give torso a bit of dynamic movement to make it expressive
+
+        // Update virtual 'foot' position, just for animation, wildly inaccurate :)
+        var neckPosition = baseOfNeckHint.position;
+        footPosition.x += (neckPosition.x - footPosition.x) * Time.deltaTime * torsoFootCurve.Evaluate(Mathf.Abs(neckPosition.x - footPosition.x));
+        footPosition.z += (neckPosition.z - footPosition.z) * Time.deltaTime * torsoFootCurve.Evaluate(Mathf.Abs(neckPosition.z - footPosition.z));
+        footPosition.y = 0;
+
+        // Forward direction of torso is vector in the transverse plane
+        // Determined by head direction primarily, hint provided by hands
+        var torsoRotation = Quaternion.identity;
+
+        // Head: Just use head direction
+        var headFwd = head.forward;
+        headFwd.y = 0;
+
+        var rot = Quaternion.LookRotation(headFwd, Vector3.up);
+        var angle = Quaternion.Angle(torsoFacing, rot);
+        var rotateAngle = Mathf.Clamp(Time.deltaTime * torsoFacingCurve.Evaluate(Mathf.Abs(angle)), 0, angle);
+        torsoFacing = Quaternion.RotateTowards(torsoFacing, rot, rotateAngle);
+
+        // Place torso so it makes a straight line between neck and feet
+        torso.position = neckPosition;
+        torso.rotation = Quaternion.FromToRotation(Vector3.down, footPosition - neckPosition) * torsoFacing;
+    }
+}
---
+++ Assets/AvatarGame/TexturedAvatar.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using Ubiq.Spawning;
+using Ubiq.Avatars;
+using UnityEngine.Events;
+using UnityEngine;
+using Avatar = Ubiq.Avatars.Avatar;
+using Ubiq.Rooms;
+using Ubiq.Messaging;
+using System.Collections;
+
+/// <summary>
+/// This class sets the avatar to use a specific texture. It also handles
+/// syncing the currently active texture over the network using properties.
+/// </summary>
+public class TexturedAvatar : MonoBehaviour
+{
+    public AvatarTextureCatalogue Textures;
+    public bool RandomTextureOnSpawn;
+    public bool SaveTextureSetting;
+    public GameObject Particles;
+    public GameObject Audio;
+
+    [Serializable]
+    public class TextureEvent : UnityEvent<Texture2D> { }
+    public TextureEvent OnTextureChanged;
+
+    private Avatar avatar;
+    private FloatingAvatar floatingAvatar;
+    private string uuid;
+    private string blob;
+    private RoomClient roomClient;
+
+    private Texture2D cached; // Cache for GetTexture. Do not do anything else with this; use the uuid
+
+    private void Start()
+    {
+        roomClient = NetworkScene.Find(this).GetComponentInChildren<RoomClient>();
+
+        avatar = GetComponent<Avatar>();
+        floatingAvatar = avatar.GetComponentInChildren<FloatingAvatar>();
+
+        if (avatar.IsLocal)
+        {
+            var hasSavedSettings = false;
+            if (SaveTextureSetting)
+            {
+                hasSavedSettings = LoadSettings();
+            }
+            if (!hasSavedSettings && RandomTextureOnSpawn)
+            {
+                SetTexture(Textures.Get(UnityEngine.Random.Range(0, Textures.Count)));
+            }
+        }
+
+        if (!avatar.IsLocal)
+        {
+            var peer = avatar.Peer;
+
+            var uuid = peer["ubiq.avatar.texture.uuid"];
+            var blob = peer["ubiq.avatar.texture.blob"];
+
+            if (!string.IsNullOrWhiteSpace(blob))
+            {
+                SetCustomTexture(blob);
+            }
+            else if (!string.IsNullOrWhiteSpace(uuid))
+            {
+                SetTexture(uuid);
+            }
+        }
+
+        roomClient.OnPeerUpdated.AddListener(RoomClient_OnPeerUpdated);
+    }
+
+    private void OnDestroy()
+    {
+        // Cleanup the event for new properties so it does not get called after
+        // we have been destroyed.
+        if (roomClient)
+        {
+            roomClient.OnPeerUpdated.RemoveListener(RoomClient_OnPeerUpdated);
+        }
+    }
+
+    void RoomClient_OnPeerUpdated(IPeer peer)
+    {
+        if (peer != avatar.Peer)
+        {
+            // The peer who is being updated is not our peer, so we can safely
+            // ignore this event.
+            return;
+        }
+        if (!string.IsNullOrWhiteSpace(peer["ubiq.avatar.texture.blob"]))
+        {
+            SetCustomTexture(peer["ubiq.avatar.texture.blob"]);
+        }
+        else
+        {
+            SetTexture(peer["ubiq.avatar.texture.uuid"]);
+        }
+    }
+
+    // The method is LOCAL, use SetCustomTexture for custom skin swampping.
+    public void SetCustomTextureTest(Texture2D texture)
+    {
+        roomClient.Me["ubiq.avatar.texture.blob"] = Texture2DToBase64(texture);
+    }
+
+
+    public void SetCustomTexture(Texture2D texture)
+    {
+        SetCustomTexture(Texture2DToBase64(texture));
+    }
+
+    public void SetCustomTexture(string blob)
+    {
+        if (String.IsNullOrWhiteSpace(blob))
+        {
+            return;
+        }
+
+        if (this.blob != blob)
+        {
+            this.blob = blob;
+            this.cached = Base64ToTexture2D(blob);
+
+            OnTextureChanged.Invoke(this.cached);
+
+            if (avatar.IsLocal)
+            {
+                roomClient.Me["ubiq.avatar.texture.blob"] = blob;
+                SpawnEffects(Particles);
+                SpawnEffects(Audio);
+            }
+
+            if (avatar.IsLocal && SaveTextureSetting)
+            {
+                SaveSettings();
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Try to set the Texture by reference to a Texture in the Catalogue. If the Texture is not in the
+    /// catalogue then this method has no effect, as Texture2Ds cannot be streamed yet.
+    /// </summary>
+    /// 
+
+    public void SetTexture(Texture2D texture)
+    {
+        SetTexture(Textures.Get(texture));
+    }
+
+    public void SetTexture(string uuid)
+    {
+        if (String.IsNullOrWhiteSpace(uuid))
+        {
+            return;
+        }
+
+        if (this.uuid != uuid)
+        {
+            var texture = Textures.Get(uuid);
+            this.uuid = uuid;
+            this.blob = null;
+            this.cached = texture;
+
+            OnTextureChanged.Invoke(texture);
+
+            if (avatar.IsLocal)
+            {
+                roomClient.Me["ubiq.avatar.texture.blob"] = null;
+                roomClient.Me["ubiq.avatar.texture.uuid"] = this.uuid;
+
+                SpawnEffects(Particles);
+                SpawnEffects(Audio);
+            }
+
+            if (avatar.IsLocal && SaveTextureSetting)
+            {
+                SaveSettings();
+            }
+        }
+    }
+
+    private void SaveSettings()
+    {
+        PlayerPrefs.SetString("ubiq.avatar.texture.uuid", uuid);
+        PlayerPrefs.SetString("ubiq.avatar.texture.blob", blob);
+    }
+
+    private bool LoadSettings()
+    {
+        var uuid = PlayerPrefs.GetString("ubiq.avatar.texture.uuid", "");
+        var blob = PlayerPrefs.GetString("ubiq.avatar.texture.blob", "");
+
+        if (!string.IsNullOrWhiteSpace(blob))
+        {
+            SetCustomTexture(blob);
+        }
+        else
+        {
+            SetTexture(uuid);
+        }
+        return !string.IsNullOrWhiteSpace(uuid) || !string.IsNullOrWhiteSpace(blob);
+    }
+
+    public void ClearSettings()
+    {
+        PlayerPrefs.DeleteKey("ubiq.avatar.texture.uuid");
+    }
+
+    public Texture2D GetTexture()
+    {
+        if (cached == null)
+        {
+            if (!string.IsNullOrWhiteSpace(blob))
+            {
+                cached = Base64ToTexture2D(blob);
+            }
+            else if (!string.IsNullOrWhiteSpace(uuid))
+            {
+                cached = Textures.Get(uuid);
+            }
+        }
+        return cached;
+    }
+
+    private Texture2D Base64ToTexture2D(string base64)
+    {
+        byte[] pngData = Convert.FromBase64String(base64);
+        Texture2D texture = new Texture2D(2, 2);
+        texture.LoadImage(pngData);
+        return texture;
+    }
+
+    private string Texture2DToBase64(Texture2D texture)
+    {
+        byte[] pngData = texture.EncodeToPNG();
+        string base64 = Convert.ToBase64String(pngData);
+        return base64;
+    }
+
+    private void SpawnEffects(GameObject particlePrefab)
+    {
+        if (particlePrefab)
+        {
+            var instance = NetworkSpawnManager.Find(this).SpawnWithPeerScope(particlePrefab);
+
+            instance.transform.position = floatingAvatar.torso.position;
+        }
+    }
+
+}
+
